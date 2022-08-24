67e3de1869b62a1426fdf0eb16d8ae4f11f13e2c
Gc96 <111275475+Gc96@users.noreply.github.com>
2022-08-24 16:28:02 +0800

    Add files via upload

---
+++ 02nodefunction.js
@@ -0,0 +1,18 @@
+// generic function definition
+function welcomeBasic(){
+    console.log("I am in space craft!")
+}
+
+// arrow function definition
+const welcomeArrow=()=>{
+    console.log("I am in aline space craft")
+
+}
+
+const welcomeArrowMessage=(message)=>{
+    console.log(message)
+}
+
+welcomeBasic()
+welcomeArrow()
+welcomeArrowMessage("Journey to Mars is started!")
